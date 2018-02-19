b976faf3b892c8a706943930f758772bb1f07419
Lawrence Manning <lawrence@aslak.net>
2018-02-19 22:15:40 +0000

    Moved CFLAGS etc into a common header
    Use tfr to get the 16bit param for get/putstr since we know the param
    will be in x.

--- user/c/libc/maxi09os.c
+++ user/c/libc/maxi09os.c
@@ -1,18 +1,18 @@
 void m_putstr(char *string)
 {
 	asm(
-	"ldy %[in]\n\t"
+	"tfr x,y\n\t"
 	"jsr [0xc060]\n\t"
-	: : [in] "" (string) : "y"
+	: :  : "y"
 	);
 }
 
 void m_getstr(char *string)
 {
 	asm(
-	"ldy %[in]\n\t"
+	"tfr x,y\n\t"
 	"jsr [0xc064]\n\t"
-	: : [in] "" (string) : "y"
+	: : : "y"
 	);
 }
 
